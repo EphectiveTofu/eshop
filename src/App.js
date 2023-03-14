1ab54ea64a226f8400f2ed70074828fa94872002
Ken <kmartinez@sjcoe.net>
2023-03-13 21:06:17 -0700

    Added Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Header from './components/header/header';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/header/header';
+import Home from './components/homepage/home'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 }
